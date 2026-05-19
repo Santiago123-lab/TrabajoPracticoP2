b044296edb6cbb1643fc660d77a4281d3f3b3ffa
Santiago Gonzalez <santiago.gonzalez157@hotmail.com>
2026-05-19 16:44:31 -0300

    Primer actualizacion del tp/subida del proyecto

---
+++ src/clases/Usuario.java
@@ -0,0 +1,20 @@
+package clases;
+
+public class Usuario {
+	
+	//String dni, String nombre, String telefono, String email
+	private String dni;
+	private String nombre;
+	private String telefono;
+	private String email;
+	
+	public Usuario(String dni, String nombre, String telefono, String email) {
+		
+		this.dni=dni;
+		this.nombre=nombre;
+		this.telefono=telefono;
+		this.email=email;
+	}
+	
+
+}
