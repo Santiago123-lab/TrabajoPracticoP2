5a7493f92bd9c1721a5c2c929a45dbc6936bd32c
Santiago Gonzalez <santiago.gonzalez157@hotmail.com>
2026-05-22 20:47:50 -0300

    agregado BilleteraTest, Empresa, y los 3 primeros metodos de Billetera

--- src/clases/Billetera.java
+++ src/clases/Billetera.java
@@ -1,30 +1,108 @@
 package clases;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Billetera implements IBilletera {
 	
-	private List <Usuario> usuarios;
-	//private List<Empresa> empresas;
-	//private List<Actividad> actividades;
+	private HashMap<String, Usuario> usuarios;
+	private HashMap<String, Empresa> empresas;
+	
+	public Billetera() {
+		
+        this.usuarios = new HashMap<>();
+        this.empresas = new HashMap<>();
+		
+	}
+	
 
 	@Override
 	public void registrarEmpresa(String cuit, String nombreFantasia, String telefono, String email,
 			String nombreContacto) {
-		// TODO Auto-generated method stub
+		
+		if(cuit==null || cuit.isBlank()) {
+			throw new IllegalArgumentException ("Ingrese un CUIT correcto.");
+		}
+		
+		if(nombreFantasia==null || nombreFantasia.isBlank()) {
+			throw new IllegalArgumentException ("Por favor, ingrese el nombre de la empresa.");
+		}
+		
+		if(telefono==null || telefono.isBlank()) {
+			throw new IllegalArgumentException ("Por favor, ingrese un telefono de contacto.");
+		}
+		
+		if(email==null || email.isBlank()) {
+			throw new IllegalArgumentException ("Por favor, ingrese un email de contacto.");
+		}
+		
+		if(nombreContacto==null || nombreContacto.isBlank()) {
+			throw new IllegalArgumentException ("Por favor, ingrese un nombre de contacto correcto.");
+		}
+		
+		if(this.empresas.containsKey(cuit)) {
+			
+			throw new IllegalArgumentException("El CUIT: "+cuit+", ya esta registrado en el sistema");
+		}
+		
+		Empresa nuevaEmpresa = new Empresa(cuit,nombreFantasia,telefono,email,nombreContacto);
+		this.empresas.put(cuit, nuevaEmpresa);
+		
 		
 	}
 
 	@Override
 	public void agregarPersonaAutorizada(String cuitEmpresa, String dniAutorizado) {
-		// TODO Auto-generated method stub
+		
+		if(cuitEmpresa== null || cuitEmpresa.isBlank()) {
+			throw new IllegalArgumentException("Por favor, ingrese un CUIT correcto.");
+			
+		}
+		
+		if(dniAutorizado == null || dniAutorizado.isBlank()) {
+			
+			throw new IllegalArgumentException("Por favor, ingrese un DNI correcto.");
+			
+		}
+		
+		if(!(this.empresas.containsKey(cuitEmpresa))) {
+			
+			throw new IllegalArgumentException("El CUIT de la empresa no esta registrado.");
+		}
+		
+		Empresa e = this.empresas.get(cuitEmpresa);
+		
+		e.agregarAutorizado(dniAutorizado);
 		
 	}
 
 	@Override
 	public void registrarUsuario(String dni, String nombre, String telefono, String email) {
-		Usuario nuevo = new Usuario(dni, nombre, telefono, email);
-		usuarios.add(nuevo);
+		
+		if(dni==null || dni.isBlank()) {
+			throw new IllegalArgumentException ("El DNI ingresado no es valido");
+
+		}
+		if(nombre==null || nombre.isBlank()) {
+			throw new IllegalArgumentException ("Por favor, ingrese un nombre valido");
+
+		}
+		if(telefono==null || telefono.isBlank()) {
+			throw new IllegalArgumentException ("El telefono ingresado no es valido");
+
+		}
+		if(email==null || email.isBlank()) {
+			throw new IllegalArgumentException ("El email ingresado no es valido");
+
+		}
+		if(usuarios.containsKey(dni)) {
+			throw new IllegalArgumentException ("El usuario con el DNI:"+dni+", ya esta ingresado en el sistema");
+
+		}
+		
+		Usuario nuevoUsuario = new Usuario(dni, nombre, telefono, email);
+		usuarios.put(dni, nuevoUsuario);
+
 	}
 
 	@Override
