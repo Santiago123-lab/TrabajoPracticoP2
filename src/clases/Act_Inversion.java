ab509a6908ba9b7698289531d184f075b25bb5bb
Santiago Gonzalez <santiago.gonzalez157@hotmail.com>
2026-05-23 05:40:02 -0300

    Agregadas la clase Actividad, sus hijas, y metodos toString

---
+++ src/clases/Act_Inversion.java
@@ -0,0 +1,63 @@
+package clases;
+
+public class Act_Inversion extends Actividad {
+	
+	private String tipoInversion;
+	private int plazo;
+	private String cvu;
+	
+	public Act_Inversion(String dniUsuario, String cvu, String tipoInversion, int plazo, double monto, String estado) {
+		
+		super(dniUsuario,monto,estado);
+		
+		this.tipoInversion=tipoInversion;
+		this.cvu=cvu;
+		this.plazo=plazo;
+		
+	}
+	
+	public String toString() {
+		
+//	     * - inversion:
+//	         * ```
+//	         * origen: [dni] ([cvu])
+//	         * desc: [tipo inversion]
+//	         * monto: [monto]
+//	         * plazo: [plazo]
+//	         * [Aprovado/Rechazado]
+		
+		StringBuilder st= new StringBuilder();
+		
+		st.append("===============\n");
+		st.append(" Inversion: \n");
+		st.append("===============\n");
+		st.append("Origen: [");
+		st.append(this.dniOrigen);
+		st.append("] ([");
+		st.append(this.cvu);
+		st.append("])\n");
+		
+		st.append("Desc: [");
+		st.append(this.tipoInversion);
+		st.append("])\n");
+		
+		st.append("Monto: [");
+		st.append(this.monto);
+		st.append("]\n");
+		
+		st.append("Plazo: [");
+		st.append(this.plazo);
+		st.append("]\n");
+		
+		st.append("[");
+		st.append(this.estado);
+		st.append("]\n");
+		
+		st.append("===============\n");
+		
+		return st.toString();
+
+		
+	}
+
+}
---
+++ src/clases/Act_Transferencia.java
@@ -0,0 +1,51 @@
+package clases;
+
+public class Act_Transferencia extends Actividad{
+	
+	private String dniDestino;
+	private String cvuOrigen;
+	private String cvuDestino;
+	
+	public Act_Transferencia(String dniOrigen, String dniDestino, String cvuOrigen, String cvuDestino, double monto, String estado) {
+		super(dniOrigen,monto,estado);
+		this.dniDestino=dniDestino;
+		this.cvuOrigen=cvuOrigen;
+		this.cvuDestino=cvuDestino;
+		
+	}
+	
+	public String toString() {
+		
+		StringBuilder st= new StringBuilder();
+		
+		st.append("* ===============\n");
+		st.append("- Transferencia: \n");
+		st.append("* ===============\n");
+		st.append("* Origen: [");
+		st.append(this.dniOrigen);
+		st.append("] ([");
+		st.append(this.cvuOrigen);
+		st.append("])\n");
+		
+		st.append("* Destino: [");
+		st.append(this.dniDestino);
+		st.append("] ([");
+		st.append(this.cvuDestino);
+		st.append("])\n");
+		
+		st.append("* Monto: [");
+		st.append(this.monto);
+		st.append("]\n");
+		
+		st.append("[");
+		st.append(this.estado);
+		st.append("]\n");
+		
+		st.append("* ===============\n");
+		
+		return st.toString();
+
+		
+	}
+
+}
